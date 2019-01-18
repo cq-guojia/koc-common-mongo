2d3aad34c0c3a4842b24ad5d20d7282720995fc0
wangdonghui <wangdonghui83@vip.qq.com>
2019-01-18 13:33:27 +0800

    Update

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const Mongoose = require('mongoose');
+const Mongoose = require('mongoose')
 
-const KOCReturn = require('koc-common-return');
+const KOCReturn = require('koc-common-return')
 
-let clientList = {};
+let clientList = {}
 
 const KOCMongo = {
   // region Init:初始化
   Init: (dblist) => {
     if (!dblist) {
-      return;
+      return
     }
     if (!(dblist instanceof Array)) {
-      dblist = [dblist];
+      dblist = [dblist]
     }
     dblist.forEach((ThisValue) => {
       try {
@@ -21,97 +21,111 @@ const KOCMongo = {
           config: {
             autoIndex: false,
           },
-        };
-        let Auth = '';
-        if (ThisValue.user && ThisValue.password) {
-          Auth = ThisValue.user + ':' + ThisValue.password + '@';
         }
-        let ConnectString = 'mongodb://' + Auth + ThisValue.uri + ':' + ThisValue.port + '/' + ThisValue.database;
+        let Auth = ''
+        if (ThisValue.user && ThisValue.password) {
+          Auth = ThisValue.user + ':' + ThisValue.password + '@'
+        }
+        let ConnectString = 'mongodb://' + Auth + ThisValue.uri + ':' + ThisValue.port + '/' + ThisValue.database
         // Use native promises
-        Mongoose.Promise = global.Promise;
-        clientList[ThisValue.name] = Mongoose.createConnection(ConnectString, Options);
+        clientList[ThisValue.name] = Mongoose.createConnection(ConnectString, Options)
       } catch (ex) {
       }
-    });
-    return clientList;
+    })
+    return clientList
   },
   // endregion
   // region Model:取得Model
-  Model: (db, name, obj) => {
-    return db.model(name, new Mongoose.Schema(obj), name);
+  Model: (db, name, obj, indexes) => {
+    const schema = new Mongoose.Schema(obj)
+    if (indexes && Array.isArray(indexes)) {
+      for (const ThisValue of indexes) {
+        schema.index(ThisValue.Index, ThisValue.Option)
+      }
+    }
+    return db.model(name, schema, name)
+  },
+  // endregion
+  // region
+  CreateIndexes: (model, option) => {
+    if (!model || !model.hasOwnProperty('createIndexes')) return KOCReturn.Value({hasError: true, message: 'model error.'})
+    option = option || {}
+    return KOCReturn.Promise(() => {
+      return model.createIndexes(option)
+    })
   },
   // endregion
   // region PageParm:分页，参数
-  PageParm: function() {
-    this.GetPageInfo = true;
-    this.OrderName = '';
-    this.Start = 1;
-    this.Length = 0;
+  PageParm: function () {
+    this.GetPageInfo = true
+    this.OrderName = ''
+    this.Start = 1
+    this.Length = 0
   },
   // endregion
   // region PageInfo:分页，页数据
   PageInfo: async (model, criteria) => {
-    let RecordCount = 0;
+    let RecordCount = 0
     let retValue = await KOCReturn.Promise(() => {
-      return model.count(criteria);
-    });
+      return model.count(criteria)
+    })
     if (!retValue.hasError) {
-      RecordCount = retValue.returnObject;
+      RecordCount = retValue.returnObject
     }
     if (!RecordCount) {
       return {
         RecordCount: 0,
         MaxCode: '',
-      };
+      }
     }
     retValue = await KOCReturn.Promise(() => {
-      return model.findOne(criteria);
-    });
+      return model.findOne(criteria)
+    })
     if (retValue.hasError) {
       return {
         RecordCount: 0,
         MaxCode: '',
-      };
+      }
     }
     return {
       RecordCount: RecordCount,
       MaxCode: retValue.returnObject._id,
-    };
-    return retValue;
+    }
+    return retValue
   },
   // endregion
   // region PageList:分页数据
   PageList: async (model, criteria, pageparm) => {
     return await KOCReturn.Promise(() => {
-      let Sort = null;
+      let Sort = null
       if (pageparm.OrderName) {
-        Sort = {};
-        pageparm.OrderName = pageparm.OrderName.split(',');
+        Sort = {}
+        pageparm.OrderName = pageparm.OrderName.split(',')
         for (let ThisValue of pageparm.OrderName) {
-          ThisValue = ThisValue.split(' ');
+          ThisValue = ThisValue.split(' ')
           ThisValue.length === 2 &&
-          (Sort[ThisValue[0]] = ThisValue[1].toLowerCase());
+          (Sort[ThisValue[0]] = ThisValue[1].toLowerCase())
         }
       }
-      let Query = model.find(criteria);
-      Sort && (Query.sort(Sort));
-      pageparm.Start && (Query.skip(pageparm.Start));
-      return Query.limit(pageparm.Length);
-    });
+      let Query = model.find(criteria)
+      Sort && (Query.sort(Sort))
+      pageparm.Start && (Query.skip(pageparm.Start))
+      return Query.limit(pageparm.Length)
+    })
   },
   // endregion
   // region Page:分页
   Page: async (model, criteria, max, pageparm) => {
-    !criteria && (criteria = {});
-    max && (criteria._id = {$gte: max});
-    let retValue = await KOCMongo.PageList(model, criteria, pageparm);
+    !criteria && (criteria = {})
+    max && (criteria._id = {$gte: max})
+    let retValue = await KOCMongo.PageList(model, criteria, pageparm)
     if (!pageparm.GetPageInfo || retValue.hasError) {
-      return retValue;
+      return retValue
     }
-    retValue.PutValue('PageInfo', await KOCMongo.PageInfo(model, criteria));
-    return retValue;
+    retValue.PutValue('PageInfo', await KOCMongo.PageInfo(model, criteria))
+    return retValue
   },
   // endregion
-};
+}
 
-module.exports = KOCMongo;
+module.exports = KOCMongo
